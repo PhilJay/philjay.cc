512312d488ac79163013895cc7769a4ae7e30b8c
Philipp Jahoda <phil.jahoda@gmail.com>
2020-06-20 22:05:25 +0200

    Add project url

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Project.css'
 
+const click = (project) => {
+    window.open(project.url, "_blank");
+}
+
 function Project(props) {
-    return <div className="project">
+    return <div className="project" onClick={() => { click(props.project) }}>
         <div className="project-content">
             <img className="project-image" src={props.project.image} />
             <h2>{props.project.name}</h2>
