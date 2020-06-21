43363032b31c2977a580a7e9eca3827e77840115
Philipp Jahoda <phil.jahoda@gmail.com>
2020-06-21 11:20:04 +0200

    Updates

--- precache-manifest.45fee79abe3d1845ef2e9d5434f6e435.js
+++ precache-manifest.45fee79abe3d1845ef2e9d5434f6e435.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1e20d6ad1c4d248be855ba16f00ae2a",
+    "revision": "4ff646269ee1c9245ec4dd1587fb3135",
     "url": "/philjay.cc/index.html"
   },
   {
-    "revision": "2293749f30b5ae684e9d",
+    "revision": "318e85314cf591328921",
     "url": "/philjay.cc/static/css/main.2f2dbb60.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/philjay.cc/static/js/2.c19778ec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2293749f30b5ae684e9d",
-    "url": "/philjay.cc/static/js/main.738fa50e.chunk.js"
+    "revision": "318e85314cf591328921",
+    "url": "/philjay.cc/static/js/main.db3773c0.chunk.js"
   },
   {
     "revision": "ed62030cbd3439040328",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/philjay.cc/precache-manifest.32d1bb9f504d765f4f3fbc83169ddffb.js"
+  "/philjay.cc/precache-manifest.45fee79abe3d1845ef2e9d5434f6e435.js"
 );
 
 self.addEventListener('message', (event) => {
