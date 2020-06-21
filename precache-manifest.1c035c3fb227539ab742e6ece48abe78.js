9566fd9743e4d68439da8889450b6800102c1eca
Philipp Jahoda <phil.jahoda@gmail.com>
2020-06-21 11:28:34 +0200

    Updates

---
+++ precache-manifest.1c035c3fb227539ab742e6ece48abe78.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15deb7106f472e2eabeeaa7e8f81dcfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ee4a5b6879499cff1b",
+    "url": "/static/css/main.2f2dbb60.chunk.css"
+  },
+  {
+    "revision": "9792698f64e206ab661c",
+    "url": "/static/js/2.1285c9cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1285c9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ee4a5b6879499cff1b",
+    "url": "/static/js/main.b14de9d1.chunk.js"
+  },
+  {
+    "revision": "a342ddb5f3d1a0861dc0",
+    "url": "/static/js/runtime-main.fb89ea21.js"
+  },
+  {
+    "revision": "cf5a941b2be5b7ecb75db1385ba1c47f",
+    "url": "/static/media/burnd.cf5a941b.png"
+  },
+  {
+    "revision": "c7719aa6e2a29d0744671251038b148e",
+    "url": "/static/media/butleroy.c7719aa6.png"
+  },
+  {
+    "revision": "20d6021b070683a4ad0110a00bc56504",
+    "url": "/static/media/jwt.20d6021b.png"
+  },
+  {
+    "revision": "9115cf25cf45a0d2aaecb2435294a71f",
+    "url": "/static/media/mpandroidchart.9115cf25.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/philjay.cc/precache-manifest.3dba71681b240ba9738c4c2c2f911bb5.js"
+  "/precache-manifest.1c035c3fb227539ab742e6ece48abe78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/philjay.cc/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
